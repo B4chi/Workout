30307fee78ea49276364020d32cb064eeb8dcfed
B4chi <bachi.paule@gmail.com>
2016-09-28 22:54:48 +0200

    Test

--- app/src/main/java/de/b4chi/workout/NewExersiceActivity.java
+++ app/src/main/java/de/b4chi/workout/NewExersiceActivity.java
@@ -41,7 +41,7 @@ public class NewExersiceActivity extends AppCompatActivity {
         String[] nums = {"1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6","6.5","7","7.5","8","8.5","9"};
         numberPicker.setDisplayedValues(nums);
         relativeLayoutRoot.addView(numberPicker);
-        System.out.println("Test");
+
 
 
     }
