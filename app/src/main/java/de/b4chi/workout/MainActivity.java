b2392fee3dad50dcaefe37c51eee1c263cfec273
B4chi <bachi.paule@gmail.com>
2016-09-28 21:25:28 +0200

    Add Missing Files

---
+++ app/src/main/java/de/b4chi/workout/MainActivity.java
@@ -0,0 +1,29 @@
+package de.b4chi.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.NewExercice){
+            // TODO: 28.09.2016  
+        }
+        return true;
+    }
+}
