936572618667c63be4196b5dbcf020f2432f63b2
B4chi <bachi.paule@gmail.com>
2016-09-28 21:28:21 +0200

    Add NewExersice Activity

---
+++ app/src/main/java/de/b4chi/workout/NewExersiceActivity.java
@@ -0,0 +1,13 @@
+package de.b4chi.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewExersiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_exersice);
+    }
+}
